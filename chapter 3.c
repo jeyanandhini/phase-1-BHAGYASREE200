33524009262e23941049c245e1ffb85cc1b56c9f
AZMEERA BHAGYA SREE <85113970+BHAGYASREE200@users.noreply.github.com>
2021-08-09 13:19:12 +0530

    Update chapter 3.c

--- chapter 3.c
+++ chapter 3.c
@@ -158,3 +158,20 @@ int main()
 
     return 0;
 }
+
+
+(d) Write a program to print all the ASCII values and their equivalent
+characters using a while loop. The ASCII values vary from 0 to 255.
+   
+   #include<stdio.h>
+int main()
+{
+    int count=0;
+    while(count<=255)
+    {
+        printf("ascii value of %c is %d\n",count,count);
+        count++;
+    }
+    return 0;
+}
+   
