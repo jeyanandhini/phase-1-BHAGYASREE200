d8e57027dbb3fe0c520a50d0803533668bea1ef9
AZMEERA BHAGYA SREE <85113970+BHAGYASREE200@users.noreply.github.com>
2021-08-09 13:33:34 +0530

    Update chapter 3.c

--- chapter 3.c
+++ chapter 3.c
@@ -192,6 +192,23 @@ int main()
     printf("%d is raised to %d:%d",a,b,pow);
     return 0;
 }
-   
-   
 
+(b) Write a program to find the factorial value of any number entered
+through the keyboard.   
+   
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num, fact=1, n;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    n = num;
+    while (num!=0)
+    {
+        fact = fact*num;
+        num = num-1;
+    }
+
+    printf("The Factorial for %d is %d", n, fact);
+}
