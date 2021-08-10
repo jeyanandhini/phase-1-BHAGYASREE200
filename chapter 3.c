305169bae7ddf97c4412459a4c49c9405043ecc1
AZMEERA BHAGYA SREE <85113970+BHAGYASREE200@users.noreply.github.com>
2021-08-10 11:30:48 +0530

    Update chapter 3.c

--- chapter 3.c
+++ chapter 3.c
@@ -273,5 +273,25 @@ int main()
     }  
   
     return 0;  
+} 
+
+
+Write a program to print the multiplication table of the number
+entered by the user. The table should get displayed in the
+following form:
+29 * 1 = 29
+29 * 2 = 58
+
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("enter number n\n");
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+        printf("\n%d * %d = %d",n,i,n*i);
+    }
+    return 0;
 }
 
