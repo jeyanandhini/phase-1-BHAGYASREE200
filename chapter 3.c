01b04f1467235c74908425f3fd065035283d8a4e
AZMEERA BHAGYA SREE <85113970+BHAGYASREE200@users.noreply.github.com>
2021-08-10 10:57:46 +0530

    Update chapter 3.c

--- chapter 3.c
+++ chapter 3.c
@@ -211,4 +211,20 @@ int main()
     }
 
     printf("The Factorial for %d is %d", n, fact);
-}
+}   
+
+Write a program to fill the entire screen with a smiling face. The
+smiling face has an ASCII value 1.     
+   
+  #include<stdio.h>                               
+   int main()                          
+ {                      
+    int i;                         
+    for(i = 0; i <= 5000; i++)                             
+    {                                 
+        printf("%c", 1);                           
+    }                               
+    return 0;                      
+}      
+   
+
