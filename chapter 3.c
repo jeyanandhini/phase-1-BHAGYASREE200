59f7b47940f7e74bcac5044c0d95be76f43ce177
AZMEERA BHAGYA SREE <85113970+BHAGYASREE200@users.noreply.github.com>
2021-08-11 12:09:50 +0530

    Update chapter 3.c

--- chapter 3.c
+++ chapter 3.c
@@ -327,4 +327,31 @@ continue;
 
 }
 
+ print right triangle star pattern series  
+ 
+ #include<stdio.h>
+ int main()
+ {
+    int i , j , rows ;
+    printf("enter rows\n ");
+    scanf("%d",&rows);
+    for(i = 1 ; i<= rows ; i++ )
+    {
+       for (j = 1 ;j<=i ; j++)
+       {
+          printf("*");
+       }
+       printf("\n");
+    }
+     return 0;
+ }
 
+//output
+enter rows
+ 5
+*
+**
+***
+****
+*****
+    
