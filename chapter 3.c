17c96f2c42c304f7ed26d1b4bb6e5f4763b53963
AZMEERA BHAGYA SREE <85113970+BHAGYASREE200@users.noreply.github.com>
2021-08-11 12:25:29 +0530

    Update chapter 3.c

--- chapter 3.c
+++ chapter 3.c
@@ -355,3 +355,32 @@ enter rows
 ****
 *****
     
+equilateral triangle programming 
+#include <stdio.h>
+
+int main() {
+   int rows,i,j;
+printf("enter rows\n");
+scanf("%d",&rows);
+
+   for(i = 1; i <= rows; i++)
+   {
+      for(j = 1; j <= rows-i; j++)
+         printf(" ");
+
+      for(j = 1; j <= i; j++)
+         printf("* ");
+
+      printf("\n");
+   }
+   return 0;
+}   
+//output  
+enter rows
+5
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+   
