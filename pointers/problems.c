c5d79889400ce8c3a95b9a135dd88f084fe5b951
AZMEERA BHAGYA SREE <85113970+BHAGYASREE200@users.noreply.github.com>
2021-08-16 13:10:50 +0530

    Create problems.c

---
+++ pointers/problems.c
@@ -0,0 +1,13 @@
+ pointers 
+           -------
+           |  i  |  👉 location name            
+           ------- 
+           |  5  |  👉 value at location   
+           -------                  
+           |65524|  👉 address of location    
+           -------         
+           
+  •  ‘&’ used in   C’s ‘address of’ operator.    
+  •   The other pointer operator available in C is ‘*’, called ‘value at address’ operator.      
+  
+           
