a1a36cf674b53204e8a3c68c115bad8383c875cd
AZMEERA BHAGYA SREE <85113970+BHAGYASREE200@users.noreply.github.com>
2021-08-16 13:17:25 +0530

    Update problems.c

--- pointers/problems.c
+++ pointers/problems.c
@@ -8,6 +8,20 @@
            -------         
            
   •  ‘&’ used in   C’s ‘address of’ operator.    
-  •   The other pointer operator available in C is ‘*’, called ‘value at address’ operator.      
+  •   The other pointer operator available in C is ‘*’, called ‘value at address’ operator.    
   
-           
+  
+  
+# include <stdio.h>
+int main( )
+{
+int i = 100;
+printf ( "Address of i = %d\n", &i ) ;
+printf ( "Value of i = %d\n", i ) ;
+printf ( "Value of i = %d\n", *( &i ) ) ;
+return 0 ;
+}    
+// output  
+Address of i = 1710818788  
+Value of i = 100  
+Value of i = 100
