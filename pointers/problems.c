cc159b2f3405102d274b722ad0350b141d04ef58
AZMEERA BHAGYA SREE <85113970+BHAGYASREE200@users.noreply.github.com>
2021-08-16 13:27:42 +0530

    Update problems.c

--- pointers/problems.c
+++ pointers/problems.c
@@ -25,3 +25,31 @@ return 0 ;
 Address of i = 1710818788  
 Value of i = 100  
 Value of i = 100
+ 
+ 
+ 
+ 
+ 
+ # include <stdio.h>
+int main( )
+{
+int i = 100;
+int  *j ;
+
+j = &i;
+printf ( "Address of i = %d\n", &i ) ;
+printf ( "Value of i = %d\n", i ) ;
+printf ( "Value at address  = %d\n", *( &i ) ) ;
+printf("address of j = %d\n", &j);
+printf("value of j = %d\n", j);
+printf("value at address = %d\n",*j);
+return 0 ;
+}
+
+//output 
+Address of i = 460806652
+Value of i = 100
+Value at address  = 100
+address of j = 460806656
+value of j = 460806656
+value at address = 100  
