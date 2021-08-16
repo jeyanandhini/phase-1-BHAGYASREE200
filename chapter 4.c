61b45c3961c199a57672753af55d1fcd701976c7
AZMEERA BHAGYA SREE <85113970+BHAGYASREE200@users.noreply.github.com>
2021-08-16 15:37:45 +0530

    Update chapter 4.c

--- chapter 4.c
+++ chapter 4.c
@@ -41,7 +41,25 @@ max value is 200
 
 
 
+#include <stdio.h>
 
+     int sum (int a, int b) 
+    {
+	    int s;
+		s=a+b;
+        return s; 
+    }
+    int main (void)
+    {
+        int total;
+		
+        total = sum (5, 6);
+        printf ("The total is :  %d\n", total);
+        return 0;
+    }
+    
+    //output 
+the total is 11
 
 
 
