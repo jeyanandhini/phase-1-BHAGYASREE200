1c8cd960e45b55c6241bc5bbb3da9139b5c07733
AZMEERA BHAGYA SREE <85113970+BHAGYASREE200@users.noreply.github.com>
2021-08-19 13:36:58 +0530

    Create q2.c

---
+++ chapter 2/q2.c
@@ -0,0 +1,30 @@
+
+ Any integer is input through the keyboard. Write a program to find
+out whether it is an odd number or even number.                
+
+#include<stdio.h>            
+int main()                
+{                  
+    int n;                   
+    printf("enter n\n");               
+    scanf("%d",&n);              
+    if(n%2==0)                  
+    {                   
+        printf("even");                 
+    }                       
+    else                  
+    {                  
+         printf("odd");            
+    }               
+    return 0;                   
+ } 
+
+
+
+
+
+
+
+
+
+
