75b1cb34247de898932ea18b667562aaa9c84592
AZMEERA BHAGYA SREE <85113970+BHAGYASREE200@users.noreply.github.com>
2021-09-10 15:04:47 +0530

    Update insertion sorting .c

--- arrays/insertion sorting .c
+++ arrays/insertion sorting .c
@@ -2,33 +2,33 @@
 
 #include<stdio.h>
 int main()
-{
-int i,j,n,temp,arr[100];
-printf("Please enter the total count of the elements that you want to sort: \n");
-scanf("%d",&n);
-printf("Please input the elements that has to be sorted:\n");
-for(i=0;i<n;i++)
-{
-scanf("%d",&arr[i]);
-}
-for(i=1;i<=n-1;i++)
-{
-temp=arr[i];
-j=i-1;
-while((temp<arr[j])&&(j>=0))
-{
-arr[j+1]=arr[j];
-j=j-1;
-}
-arr[j+1]=temp;
-}
-printf("\n Output generated after using insertion sort \n");
-for(i=0;i<n;i++)
-{
-printf("%d ",arr[i]);
-}
-return 0;
-}
+    {
+    int i,j,n,temp,arr[100];
+    printf("Please enter the total count of the elements that you want to sort: \n");
+    scanf("%d",&n);
+    printf("Please input the elements that has to be sorted:\n");
+    for(i=0;i<n;i++)
+      {
+        scanf("%d",&arr[i]);
+      }
+    for(i=1;i<=n-1;i++)
+      {
+        temp=arr[i];
+        j=i-1;
+       while((temp<arr[j])&&(j>=0))
+           {
+             arr[j+1]=arr[j];
+             j=j-1;
+            }
+       arr[j+1]=temp;
+       }
+    printf("\n Output generated after using insertion sort \n");
+    for(i=0;i<n;i++)
+    {
+    printf("%d ",arr[i]);
+    }
+    return 0;
+    }
 
 //output//
 Please enter the total count of the elements that you want to sort:
