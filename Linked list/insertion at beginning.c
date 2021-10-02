c45ae41ffe31bf3bbfd71792bc5c624a46c6507c
AZMEERA BHAGYA SREE <85113970+Azmeerabhagyasree@users.noreply.github.com>
2021-10-02 13:16:50 +0530

    Update insertion at beginning.c

--- Linked list/insertion at beginning.c
+++ Linked list/insertion at beginning.c
@@ -2,28 +2,28 @@
 #include<stdlib.h>
 
 struct node
-{
+ {
 	int  data ;
 	struct node*next;
-};
+ };
 
 void insertion(int num,struct node**head)
-{
+     {
 	struct node* newnode;
 	newnode=(struct node*)malloc(sizeof (struct node));
 	newnode->data=num;
 	newnode->next=*head;
 	*head=newnode;
-}
+     }
 
 void printlist(struct node*n)
-{
+  {
 	while(n != NULL)
 	{
 		printf("%d\n",n->data);
 		n = n->next;
 	}
-}
+  }
 void main()
 {
 	
@@ -56,4 +56,61 @@ void main()
 
 --------------------------------
 Process exited after 0.005921 seconds with return value 0
+Press any key to continue . . .
+	
+	
+	
+	
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+#include<stdio.h>
+#include<malloc.h>
+
+
+struct node
+{
+	int data;
+	struct node* next;
+};
+struct node* head=NULL;
+
+
+struct node* getnewnode(int val)
+{
+	struct node* temp;
+	temp= (struct node*)malloc(sizeof(struct node));
+	temp->data =val;
+	temp->next= NULL;
+	return temp;
+}
+insertbeg(int val)
+{
+	struct node* ptr;
+	ptr=getnewnode(val);
+	ptr->next=head;
+	head= ptr;
+}
+printList()
+{
+	struct node* run;
+	run = head;
+	while(run != NULL)
+	{
+		printf("%d -> ",run->data);
+		run = run ->next;
+	}
+	printf("NULL");
+}
+void main()
+{
+	insertbeg(6);
+	insertbeg(5);
+	insertbeg(4);
+	printList();
+}
+
+//output
+4 -> 5 -> 6 -> NULL
+--------------------------------
+Process exited after 0.05065 seconds with return value 4
 Press any key to continue . . .
