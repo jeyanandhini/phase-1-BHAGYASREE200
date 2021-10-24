5ee7f24d2b7025955760d740460697cab3b6a7c6
AZMEERA BHAGYA SREE <85113970+Azmeerabhagyasree@users.noreply.github.com>
2021-10-24 12:46:02 +0530

    Create OctalEquivalent.c

---
+++ chapter 3/OctalEquivalent.c
@@ -0,0 +1,20 @@
+Write a program to receive an integer and find its octal equivalent.     
+
+#include <stdio.h>   
+#include<math.h>   
+
+int main()   
+{   
+    int integer,oct=0,num,i=0;   
+    printf("enter integer");   
+    scanf("%d",&integer);   
+    while(integer!=0)   
+    {                
+     num=integer%8;   
+     oct=oct+num*pow(10,i);    
+     integer=integer/8;    
+     i++;    
+    }     
+    printf("enter oct %d", oct);     
+    return 0;    
+} 
