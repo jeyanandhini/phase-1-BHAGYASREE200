1549e2ee430c5ed0d354edabf3d4a4f29dd6e190
AZMEERA BHAGYA SREE <85113970+Azmeerabhagyasree@users.noreply.github.com>
2021-10-24 12:45:24 +0530

    Create PositiveNegativeZero.c

---
+++ chapter 3/PositiveNegativeZero.c
@@ -0,0 +1,33 @@
+ Write a program to enter numbers till the user wants. At the end it
+should display the count of positive, negative and zeros entered.   
+
+#include < stdio.h >  
+  
+int main()  
+{  
+    int limit, num, positive = 0, negative = 0, zero=0;                        
+    printf("Enter the limit\n");                    
+    scanf("%d", &limit);  
+    printf("Enter %d numbers\n", limit);  
+    while(limit)  
+    {  
+        scanf("%d", &num);  
+        if(num > 0)  
+        {  
+            positive++;  
+        }  
+        else if(num < 0)  
+        {  
+            negative++;  
+        }  
+        else  
+        {  
+            zero++;  
+        }  
+        limit--;  
+    }  
+    printf("\nPositive Numbers: %d\n", positive);  
+    printf("Negative Numbers: %d\n", negative);  
+    printf("Number of zero: %d\n", zero);  
+    return 0;  
+}  
