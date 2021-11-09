6dfd617dd91b7923b7dba121a29ee8e488ca0a49
AZMEERA BHAGYA SREE <85113970+Azmeerabhagyasree@users.noreply.github.com>
2021-11-09 11:54:32 +0530

    Create NaturalNumberInIncreasingOrder.c

---
+++ DSA/Recursion/NaturalNumberInIncreasingOrder.c
@@ -0,0 +1,20 @@
+
+ 👉*program to write n natural numbers in increasing order*            
+
+
+#include <stdio.h>     
+ int fun(int n)              
+{   if (n==0)             
+     return;              
+    fun(n-1);              
+    printf("%d ",n   );   
+    }                      
+    int main()                
+{                   
+   int n;            
+   fun(5);           
+    return 0;          
+}                      
+output : 1 2 3 4 5               
+© 2021 GitHub, Inc.
+Terms
