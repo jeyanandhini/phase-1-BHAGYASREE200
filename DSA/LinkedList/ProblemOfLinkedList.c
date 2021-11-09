c35d2f29e360a55df759918261ff9c073503c679
AZMEERA BHAGYA SREE <85113970+Azmeerabhagyasree@users.noreply.github.com>
2021-11-09 11:46:22 +0530

    Create ProblemOfLinkedList.c

---
+++ DSA/LinkedList/ProblemOfLinkedList.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+ 
+int print(struct Node* n)
+{
+    while (n != NULL) 
+    {
+        printf(" %d ", n->data);
+        n = n->next;
+    }
+}
+ 
+int main()
+{
+  struct Node* head;
+  struct Node* temp;
+  temp = (struct Node*)malloc(sizeof(struct Node));
+  temp-> data = 10;
+  temp-> next = NULL;
+  head = temp;
+  struct Node* temp1;
+  temp1 = (struct Node*)malloc(sizeof(struct Node));
+  temp1-> data = 11;
+  temp1-> next = NULL;
+  temp->next=temp1;
+ 
+  print(head);
+ 
+    return 0;
+}
+
+//output
+10  11
+--------------------------------
+Process exited after 0.6084 seconds with return value 0
+Press any key to continue . . .
